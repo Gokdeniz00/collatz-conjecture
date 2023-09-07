9260d0072ab0d4880688d3a40de5c5ece763e921
Gökdeniz Dağ <mountainskysea07@gmail.com>
2023-09-07 22:05:02 +0300

    bugfixes:        modified:fixed duplicate values

--- collatz.go
+++ collatz.go
@@ -8,15 +8,17 @@ import (
 
 func main(){
 	var number int
-	fmt.Print("Enter a number for Collatz Conjecture")
+	fmt.Print("Enter a number for Collatz Conjecture:")
 	fmt.Scan(&number)
 	values := arraylist.New()
-	Collatz(number, values)
+	Collatz(number, values,true)
 	fmt.Print(values)
 
 }
-func Collatz(number int,values *arraylist.List) {
-	values.Add(number)
+func Collatz(number int,values *arraylist.List,first bool) {
+	if first{
+		values.Add(number)
+	}
 	if number % 2 == 0 {
 		number = number/2
 		values.Add(number)
@@ -27,6 +29,6 @@ func Collatz(number int,values *arraylist.List) {
 	if number==1{
 		values.Add(1)
 	}else{
-		Collatz(number,values)
+		Collatz(number,values,false)
 	}
 }
