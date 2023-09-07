202f0de92a8c00a7e631dbeeff35de44c993e32f
Gökdeniz Dağ <mountainskysea07@gmail.com>
2023-09-07 22:43:30 +0300

    started plotting

--- collatz.go
+++ collatz.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 
 	"github.com/emirpasic/gods/lists/arraylist"
+
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
 )
 
 func main(){
@@ -13,6 +16,19 @@ func main(){
 	values := arraylist.New()
 	Collatz(number, values,true)
 	fmt.Print(values)
+
+	p:=plot.New()
+	
+	p.Title.Text = "Collatz Conjecture"
+	p.X.Label.Text="Step"
+	p.Y.Label.Text="Value"
+
+	p.Add(plotter.NewGrid())
+	points:=Points(values.Size(),values.Values())
+	//graph,err:=plotter.NewScatter()
+}
+func Points(size int, values []interface{}) plotter.XYs {
+	pts:=make(plotter.XYs, size)
 
 }
 func Collatz(number int,values *arraylist.List,first bool) {
@@ -26,9 +42,7 @@ func Collatz(number int,values *arraylist.List,first bool) {
 		number=3*number+1
 		values.Add(number)
 	}
-	if number==1{
-		values.Add(1)
-	}else{
+	if number!=1{
 		Collatz(number,values,false)
 	}
 }
