d75779232868ca575d9eeb56c2b15ecf2add5637
Gökdeniz Dağ <mountainskysea07@gmail.com>
2023-09-07 21:55:18 +0300

    first commit

---
+++ collatz.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/emirpasic/gods/lists/arraylist"
+)
+
+func main(){
+	var number int
+	fmt.Print("Enter a number for Collatz Conjecture")
+	fmt.Scan(&number)
+	values := arraylist.New()
+	Collatz(number, values)
+	fmt.Print(values)
+
+}
+func Collatz(number int,values *arraylist.List) {
+	values.Add(number)
+	if number % 2 == 0 {
+		number = number/2
+		values.Add(number)
+	}else if number % 2 == 1 {
+		number=3*number+1
+		values.Add(number)
+	}
+	if number==1{
+		values.Add(1)
+	}else{
+		Collatz(number,values)
+	}
+}
